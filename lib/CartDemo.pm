ffbdeada7b2fff8bd1ae242ae119543d8d86eaa4
Andrew Baerg <andrew@pullingshots.ca>
2016-09-19 11:45:42 -0600

    reset to first step

--- lib/CartDemo.pm
+++ lib/CartDemo.pm
@@ -1,17 +1,10 @@
 package CartDemo;
 use Dancer2;
-use Dancer2::Plugin::Cart;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
-};
-
-hook 'plugin.cart.products' => sub {
-  my $ec_cart = session('ec_cart');
-  $ec_cart->{products} = [ { ec_sku => 'BOOK', ec_price => 100, description => 'The Dancer2 Book' } ];
-  session ec_cart => $ec_cart;
 };
 
 true;
